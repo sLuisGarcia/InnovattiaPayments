eabde3304f84e2c13782811d914d8037caa360f4
Luis García <jlsgarcia50@gmail.com>
2023-03-15 16:42:35 -0600

    First commit

--- Example/Pods/Target Support Files/InnovattiaPayments/InnovattiaPayments-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_InnovattiaPayments : NSObject
-@end
-@implementation PodsDummy_InnovattiaPayments
-@end
--- Example/Pods/Target Support Files/Pods-InnovattiaPayments_Example/Pods-InnovattiaPayments_Example-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_InnovattiaPayments_Example : NSObject
-@end
-@implementation PodsDummy_Pods_InnovattiaPayments_Example
-@end
